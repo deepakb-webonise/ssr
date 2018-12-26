d5bd3f2229858cd23ba12ef46e33d463677a8fb3
Deepak Bansode <deepak.bansode@weboniselab.com>
2018-12-26 12:33:17 +0530

    add helmet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <Helmet>
+      <title>Home</title>
+      <meta name="description" content="Todos on steroid!" />
+      <meta name="theme-color" content="#008f68" />
+    </Helmet>
+    <h2>Home</h2>
+    <p>This is the home page!</p>
+  </div>
+);
 
+const About = () => (
+  <div>
+    <Helmet>
+      <title>About</title>
+      <meta name="description" content="Todos on steroid!" />
+      <meta name="theme-color" content="#008f68" />
+    </Helmet>
+    <h2>About</h2>
+    <p>This is the about page!</p>
+  </div>
+);
+
+const Contact = ({ match }) => (
+  <div>
+    <Helmet>
+      <title>Contact</title>
+      <meta name="description" content="Todos on steroid!" />
+      <meta name="theme-color" content="#008f68" />
+    </Helmet>
+    <h2>Contact</h2>
+    <p>This is the contact us page!</p>
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Helmet>
+        <title>Turbo Todo</title>
+        <meta name="description" content="Todos on steroid!" />
+        <meta name="theme-color" content="#008f68" />
+      </Helmet>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
 export default App;
